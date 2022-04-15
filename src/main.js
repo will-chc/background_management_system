cec5279ac18a7cd97221c190de948269e13bde01
will-chc <miss1132381819@163.com>
2022-04-15 21:42:41 +0800

    初始化路由、axios 、elementui

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
+
+//引入路由插件
+import VueRouter from 'vue-router'
+//引入路由器
+import router from './router'
+
+//引入axios库
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 import App from './App.vue'
+
+Vue.config.productionTip =false;
+
+//使用路由插件
+Vue.use(VueRouter);
+
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router:router,
 }).$mount('#app')
---
+++ src/router/main.js
@@ -0,0 +1,12 @@
+//引入路由插件
+import VueRouter from 'vue-router';
+
+const router = new VueRouter({
+    //路由
+    routes:[
+
+    ]
+});
+
+//暴露路由器
+export default router;
